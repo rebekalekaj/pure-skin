a4edfc62884a98f8e66e40851eb11e6a2a4141e2
bernarddoci <bernard.doci@gmail.com>
2020-05-14 13:29:45 +0200

    prepared for deployment

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const store = new MongoDBStore({
 // Cross site request foregery token
 const csrfProtection = csrf();
 
-const privateKey = fs.readFileSync('server.key');
-const certificate = fs.readFileSync('server.cert');
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
 // Multer for uploading files
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
